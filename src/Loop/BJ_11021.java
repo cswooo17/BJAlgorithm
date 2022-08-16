b8bb9e8bbeabc19514831cd21ad3536ff1d96f75
c9github <c9github@gmail.com>
2022-08-16 22:42:27 +0900

    220816_commit_1

---
+++ src/Loop/BJ_11021.java
@@ -0,0 +1,18 @@
+package Loop;
+
+import java.util.Scanner;
+
+public class BJ_11021 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt();
+
+        for (int i = 1; i <= count; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            System.out.println("Case #" + i + ": " + (a + b));
+        }
+        sc.close();
+    }
+}
