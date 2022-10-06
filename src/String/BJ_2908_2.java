613e9201034bcb1b6db241939c6677d6932a578f
c9github <c9github@gmail.com>
2022-10-06 23:50:36 +0900

    백준 2908 상수_2 if문에서 삼항연산자로 변환 및 주석 추가

--- src/String/BJ_2908_2.java
+++ src/String/BJ_2908_2.java
@@ -9,17 +9,12 @@ public class BJ_2908_2 {
         int a = Reverse(sc.nextInt());
         int b = Reverse(sc.nextInt());
 
-        // if문을 썼지만 삼항 연산자를 써줘도 된다.
-        if (a > b) {
-            System.out.println(a);
-        } else if (a < b) {
-            System.out.println(b);
-        }
+        System.out.println(a > b ? a : b);
     }
 
     //Reverse 함수 생성
     static int Reverse(int num) { // static : 정적 변수
-        String number = String.valueOf(num);
+        String number = String.valueOf(num);//valueOf : 파라미터가 null이면 문자열 null을 반환한다. (toString은 NPE 발생)
         String tmp = "0";
 
         for (int i = 3; i > 0; i--) {
