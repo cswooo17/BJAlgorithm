766f3655df19a81095af61030202a6debe7940e5
c9github <c9github@gmail.com>
2022-10-16 20:53:33 +0900

    백준 2775 부녀회장 주석 추가

--- src/BasicMath1/BJ_2775.java
+++ src/BasicMath1/BJ_2775.java
@@ -6,9 +6,10 @@ public class BJ_2775 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         StringBuffer sb = new StringBuffer();
-        int T = sc.nextInt();
+        int T = sc.nextInt(); //TastCase T
         int[] people = new int[15]; // 한 층에 사는 사람들의 수
 
+        // TestCase T 만큼 반복
         for (int t = 0; t < T; t++) {
             for (int i = 0; i < people.length; i++) {
                 people[i] = i;
@@ -21,7 +22,7 @@ public class BJ_2775 {
                     people[j] += people[j - 1];
                 }
             }
-            sb.append(people[n] + "\n");
+            sb.append(people[n] + "\n"); // append : 문자열의 마지막에 이어 붙이는 메소드
         }
         System.out.println(sb);
     }
