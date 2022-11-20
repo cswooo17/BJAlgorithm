a45537fbcdffb7ee5899c1d44d1420a82dd4c5a0
c9github <c9github@gmail.com>
2022-11-20 17:40:43 +0900

    BJ_2558_BufferedReader

--- src/BronzeV/BJ_2558.java
+++ src/BronzeV/BJ_2558.java
@@ -1,13 +1,18 @@
 package BronzeV;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 
 public class BJ_2558 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+    // BufferedReader를 사용할때는 IOException 예외처리
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
 
         System.out.println(a + b);
+        br.close();
     }
 }
