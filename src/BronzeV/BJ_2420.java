d6f2474c8d0a199a8fa64b1a6f6ec8208fafc97b
c9github <c9github@gmail.com>
2022-11-22 22:59:13 +0900

    BOJ_2420_comment

--- src/BronzeV/BJ_2420.java
+++ src/BronzeV/BJ_2420.java
@@ -6,6 +6,7 @@ public class BJ_2420 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        //결과값이 int범위를 초과하기 때문에 long으로 선언
         long N = sc.nextLong();
         long M = sc.nextLong();
 
