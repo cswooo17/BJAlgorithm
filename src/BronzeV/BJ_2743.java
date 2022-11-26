62cbe284dd54b763a3faae33ec9daff6d176402b
c9github <c9github@gmail.com>
2022-11-26 15:05:13 +0900

    BOJ_2743_COMMIT

---
+++ src/BronzeV/BJ_2743.java
@@ -0,0 +1,14 @@
+package BronzeV;
+
+import java.util.Scanner;
+
+public class BJ_2743 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String word = sc.next();
+        int wordLength = word.length();
+        System.out.println(wordLength);
+
+    }
+}
