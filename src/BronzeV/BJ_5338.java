7930051cf150cefe0769db93a6198e95dd2b3bec
c9github <c9github@gmail.com>
2022-12-03 19:56:38 +0900

    BOJ_5338_COMMIT

---
+++ src/BronzeV/BJ_5338.java
@@ -0,0 +1,11 @@
+package BronzeV;
+
+public class BJ_5338 {
+    public static void main(String[] args) {
+        System.out.println("       _.-;;-._\n" +
+                           "'-..-'|   ||   |\n" +
+                           "'-..-'|_.-;;-._|\n" +
+                           "'-..-'|   ||   |\n" +
+                           "'-..-'|_.-''-._|");
+    }
+}
