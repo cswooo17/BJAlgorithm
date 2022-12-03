105ad127b5ca64197ffd4c0e8e328265b2c60f59
c9github <c9github@gmail.com>
2022-12-03 19:53:11 +0900

    BOJ_5337_COMMIT

---
+++ src/BronzeV/BJ_5337.java
@@ -0,0 +1,9 @@
+package BronzeV;
+
+public class BJ_5337 {
+    public static void main(String[] args) {
+        System.out.println(".  .   .\n" +
+                "|  | _ | _. _ ._ _  _\n" +
+                "|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
