8331b4138c95d39e801edbc05ed58db350e22811
c9github <c9github@gmail.com>
2022-12-04 17:14:04 +0900

    BOJ_2083_COMMIT

---
+++ src/BronzeIV/BJ_2083.java
@@ -0,0 +1,25 @@
+package BronzeIV;
+
+import java.util.Scanner;
+
+public class BJ_2083 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String name = sc.next();
+            int age = sc.nextInt();
+            int weight = sc.nextInt();
+
+            if (name.equals("#") && age == 0 && weight == 0) {
+                break;
+            }
+
+            if (age > 17 || weight > 79) {
+                System.out.println(name + " Senior");
+            } else {
+                System.out.println(name + " Junior");
+            }
+        }
+    }
+}
