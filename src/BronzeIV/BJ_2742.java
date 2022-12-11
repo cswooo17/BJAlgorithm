53d01b4a9fe273203c58f773e547faf749bae721
c9github <c9github@gmail.com>
2022-12-11 19:04:37 +0900

    BOJ_2742_COMMIT

---
+++ src/BronzeIV/BJ_2742.java
@@ -0,0 +1,14 @@
+package BronzeIV;
+
+import java.util.Scanner;
+
+public class BJ_2742 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for (int i = N; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
