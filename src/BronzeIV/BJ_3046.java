34c1f40d610cec55aac09ecc275a60c2efcb3560
c9github <c9github@gmail.com>
2023-01-01 20:01:52 +0900

    BOJ_3046_COMMIT

---
+++ src/BronzeIV/BJ_3046.java
@@ -0,0 +1,12 @@
+package BronzeIV;
+
+import java.util.Scanner;
+
+public class BJ_3046 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int R1 = sc.nextInt();
+        int S = sc.nextInt();
+        System.out.println(S * 2 - R1);
+    }
+}
