51ce0858c84ede469e152001ee8b22fbbaa3e8e1
c9github <c9github@gmail.com>
2023-01-02 23:25:18 +0900

    BOJ_4299_COMMENT_COMMIT

--- src/BronzeIV/BJ_4299.java
+++ src/BronzeIV/BJ_4299.java
@@ -6,8 +6,11 @@ public class BJ_4299 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        int a = sc.nextInt(); //합
+        int b = sc.nextInt(); //차
+        // 짝수여부 확인 : (a + b)를 2로 나눈 나머지가 0이 아닌 경우
+        // 점수의 합이 차보다 작은 경우
+        // 짝수가 아니거나 차가 합보다 큰 경우 -1 출력
         if ((a + b) % 2 != 0 || a < b) {
             System.out.println(-1);
         } else {
