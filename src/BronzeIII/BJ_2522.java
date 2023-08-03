70dcd0cb170214302e88a03bb692f8efa4a4d5b5
c9github <c9github@gmail.com>
2023-08-03 23:15:50 +0900

    BOJ_2522 13라인 1에서 공백으로 수정

--- src/BronzeIII/BJ_2522.java
+++ src/BronzeIII/BJ_2522.java
@@ -10,7 +10,7 @@ public class BJ_2522 {
 
         for (int i = 1; i <= N; i++) {
             for (int j = N - i; j > 0; j--) {
-                System.out.print("1");
+                System.out.print(" ");
             }
 
             for (int j = 1; j <= i; j++) {
